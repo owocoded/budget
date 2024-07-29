bee078bb6fed907ad11aa970e3b284d3d09abd5a
Yusuf Babatunde <owocoded@gmail.com>
2024-07-29 18:05:51 +0100

    update the functionalities, the style and some html tags

--- budget.js
+++ budget.js
@@ -2,6 +2,9 @@ const userInput1 = document.getElementById('userInput1');
 const userInput2 = document.getElementById('userInput2');
 const userInput3 = document.getElementById('userInput3');
 const userInput4 = document.getElementById('userInput4');
+const expense = document.getElementById('expense');
+const totalBudget = document.getElementById('totalBudget');
+const balance = document.getElementById('balance');
 const addButton = document.getElementById('addButton');
 const tableBody = document.getElementById('tableBody');
 
@@ -9,8 +12,8 @@ let expenseArray = [];
 addButton.addEventListener('click', function () {
   const name = userInput2.value;
   const amount = userInput3.value;
-  const price = userInput3.value * userInput4.value;
   const quantity = userInput4.value;
+  const price = amount * quantity;
   let expenseObject = {
     name: name,
     price: price,
@@ -24,16 +27,13 @@ addButton.addEventListener('click', function () {
     const tableRow = document.createElement('tr');
     const nameCell = document.createElement('td');
     const priceCell = document.createElement('td');
-    const quantityCell = document.createElement('td');
     const row = document.createElement('button');
 
     nameCell.textContent = (name + " " + (amount + "x" + quantity));
     priceCell.textContent = price;
-    quantityCell.textContent = quantity;
 
     tableRow.appendChild(nameCell);
     tableRow.appendChild(priceCell);
-    tableRow.appendChild(quantityCell);
     tableRow.appendChild(row);
     tableBody.appendChild(tableRow);
 
@@ -44,11 +44,19 @@ addButton.addEventListener('click', function () {
   else {
     alert('Please fill in all field');
   }
+
   tableBody.addEventListener('click', (Event) => {
     if (Event.target.tagName === 'BUTTON') {
       const row = Event.target.closest('tr');
       row.remove();
     }
   });
+
+  let expense = 0;
+  const totalExpense = document.getElementById("expense");
+  expense += parseInt(price);
+  totalExpense.textContent = expense;
+
   totalBudget.innerText = userInput1.value;
+  balance.innerText = totalBudget - expense;
 });

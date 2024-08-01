d612be7904dfa8329a7f7e11dabd1e538241aadf
Yusuf Babatunde <owocoded@gmail.com>
2024-08-01 09:11:36 +0100

    Update the functionallity and styling

--- budget.js
+++ budget.js
@@ -1,39 +1,52 @@
+let total = 0;
+let expenses = JSON.parse(localStorage.getItem('userData')) || [];
+
 const userInput1 = document.getElementById('userInput1');
 const userInput2 = document.getElementById('userInput2');
 const userInput3 = document.getElementById('userInput3');
 const userInput4 = document.getElementById('userInput4');
-const expense = document.getElementById('expense');
+const date = document.getElementById('dateInput');
 const totalBudget = document.getElementById('totalBudget');
-const balance = document.getElementById('balance');
+const totalExpense = document.getElementById('expense');
+const totalBalance = document.getElementById('balance');
 const addButton = document.getElementById('addButton');
 const tableBody = document.getElementById('tableBody');
 
-let expenseArray = [];
+
 addButton.addEventListener('click', function () {
   const name = userInput2.value;
+  const date = dateInput.value;
   const amount = userInput3.value;
   const quantity = userInput4.value;
   const price = amount * quantity;
+
   let expenseObject = {
     name: name,
-    price: price,
+    date: date,
+    amount: amount,
     quantity: quantity,
-    amount: amount
+    price: price,
   }
-  expenseArray.push(expenseObject);
-  localStorage.setItem('userData', JSON.stringify(expenseArray));
 
-  if (name !== '' && price !== '' && quantity !== '' && amount !== '') {
+  if (name !== '' && price !== '' && quantity !== '' && amount !== '' && date !== '') {
     const tableRow = document.createElement('tr');
     const nameCell = document.createElement('td');
     const priceCell = document.createElement('td');
-    const row = document.createElement('button');
-
-    nameCell.textContent = (name + " " + (amount + "x" + quantity));
+    const dateCell = document.createElement('td');
+    
+    nameCell.textContent = (name + " " + "(" + (amount + "x" + quantity) + ")");
     priceCell.textContent = price;
+    dateCell.textContent = date;
+
+    const row = document.createElement('button');
+    row.textContent = "Delete";
+    row.style.color = 'white';
+    row.style.backgroundColor = 'red';
+    row.style.cursor = 'pointer';
 
     tableRow.appendChild(nameCell);
     tableRow.appendChild(priceCell);
+    tableRow.appendChild(dateCell);
     tableRow.appendChild(row);
     tableBody.appendChild(tableRow);
 
@@ -44,6 +57,11 @@ addButton.addEventListener('click', function () {
   else {
     alert('Please fill in all field');
   }
+  expenses.push(expenseObject);
+  localStorage.setItem('userDate', JSON.stringify(expenses));
+  
+  total += price;
+  totalExpense.textContent = total;
 
   tableBody.addEventListener('click', (Event) => {
     if (Event.target.tagName === 'BUTTON') {
@@ -52,11 +70,7 @@ addButton.addEventListener('click', function () {
     }
   });
 
-  let expense = 0;
-  const totalExpense = document.getElementById("expense");
-  expense += parseInt(price);
-  totalExpense.textContent = expense;
-
   totalBudget.innerText = userInput1.value;
-  balance.innerText = totalBudget - expense;
+  remain = userInput1.value - total;
+  totalBalance.innerText = remain;
 });
